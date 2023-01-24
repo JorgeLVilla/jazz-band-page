77fce883e5d4770ecc8e7b4850e4c13e7dd3acfc
Jorge Villalobos <79444943+JorgeLVilla@users.noreply.github.com>
2023-01-23 16:29:37 -0800

    feat(hero): initialize hero page

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,17 +8,17 @@ const Navbar = ({}) => {
   const [navbarOpen, setNavbarOpen] = useState(false);
 
   return (
-    <nav className="fixed bg-black w-full top-0 right-0 left-0 z-10">
+    <nav className="fixed bg-transparent w-full top-0 right-0 left-0 z-10">
       <div className="text-white flex justify-evenly items-center">
         {/* Left side */}
         <div >
-          <h2 className="font-[Cinzel] md:text-3xl">New <br/> Vision <br/> Band</h2>
+          <h2 className="font-['Cinzel'] md:text-2xl flex flex-col"><span>New</span><span>Vision</span><span>Back</span></h2>
         </div>
         {/* Middle part */}
         <div>
           <ul className="flex">
             {NAV__LINKS.map((item) => (
-              <li className="m-5 md: text-2xl">
+              <li className="m-5 md: text-xl">
                 <Link href={item.path}>{item.display}</Link>
               </li>
             ))}
@@ -26,9 +26,9 @@ const Navbar = ({}) => {
         </div>
         {/* Social media */}
         <div className="flex gap-4">
-             <BsFacebook />       
-             <BsInstagram />       
-             <BsYoutube />       
+             <BsFacebook className="text-xl"/>       
+             <BsInstagram className="text-xl"/>       
+             <BsYoutube className="text-xl"/>       
         </div>
       </div>
     </nav>
